c52e00b4133cb80810b33969e8c582eba7884510
kuuriios <56999312+kuuriios@users.noreply.github.com>
2021-05-20 20:12:05 +0200

    Add files via upload

---
+++ GreenVirus.java
@@ -0,0 +1,16 @@
+class GreenVirus extends Enemy{
+  int hp; 
+  
+  
+  public void sethp(){
+    hp = ;
+  }
+  
+  public int gethp(){
+    return this.hp;
+  }
+  
+  public void damaged(){
+  
+  }
+}
---
+++ PinkVirus.java
@@ -0,0 +1,16 @@
+class PinkVirus extends Enemy{
+  int hp; 
+  
+  
+  public void sethp(){
+    hp = ;
+  }
+  
+  public int gethp(){
+    return this.hp;
+  }
+  
+  public void damaged(){
+  
+  }
+}

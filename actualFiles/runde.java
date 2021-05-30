08f18b2a9992632c526048d72e4baca4575ccc9c
Sailsman <danielgluch33@gmail.com>
2021-05-30 19:48:18 +0200

    loadRoundsFromFile done

--- actualFiles/runde.java
+++ actualFiles/runde.java
@@ -1,12 +1,15 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+
 class Runde {
   
   private int[] timeline;
   
   public Runde (int sekunden) {
     this.timeline = new int[sekunden * 30];
-    for (int i = 0; i < timeline.length; i++) {
-      timeline[i] = 0;
-    }
+    this.resetTimeline();
   }
   
   public void setEnemyOnFrame(int frame, int enemyID) {
@@ -23,5 +26,34 @@ class Runde {
   
   public int getRoundLength() {
     return this.timeline.length;
+  }
+  
+  private void resetTimeline() {
+    for (int i = 0; i < timeline.length; i++) {
+      timeline[i] = 0;
+    } 
+  }
+  
+  public void loadRoundFromFile(String filename) {
+    try {
+      File Runde = new File("C://Users//Daniel//Documents//GitHub//CovDefense//actualFiles//" + filename);
+      Scanner sc = new Scanner(Runde);
+      
+      this.timeline = new int[Integer.parseInt(sc.nextLine())];
+      this.resetTimeline();
+      
+      while (sc.hasNextLine()) { 
+        String text = sc.nextLine();
+        String[] split = text.split(",");
+        
+        this.setEnemyOnFrame(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
+      }  
+      
+      sc.close();
+      
+    } catch(FileNotFoundException e) {
+        System.out.println("Datei nicht da!");
+        e.printStackTrace();        
+    }
   }
 }

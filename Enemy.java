a59f4883957fed06b8f7858041833c577da90776
Sailsman <danielgluch33@gmail.com>
2021-06-17 18:02:59 +0200

    umstrukturierung

--- Enemy.java
+++ Enemy.java
@@ -1,21 +1,104 @@
-class Enemy extends Objeckt{
-  private int type;
-  private float speed;
+import javafx.scene.image.Image;
+import java.lang.Math;
+
+
+class Enemy extends Basis{
+  private int hp;
+  private int damage;
+  private int moneyValue;
+  private double speed;
+  private int target;
   
-  public int getType(){
-    return this.type;
+  public Enemy(Image sprite, double x, double y, double speed, int hp, int damage, int moneyValue) {
+    super(sprite, x, y);
+    this.getIV().setFitHeight(50);
+    this.getIV().setFitWidth(85);
+    this.speed = speed;
+    this.hp = hp;
+    this.damage = damage;
+    this.moneyValue = moneyValue;
+    this.target = 0;
   }
   
-  public float getSpeed(){
+  public int getHp(){
+    return this.hp;
+  }
+  
+  public int getDamage(){
+    return this.damage;
+  }
+  
+  public int getMoneyValue(){
+    return this.moneyValue;
+  }
+  
+  public double getSpeed(){
     return this.speed;
   }
   
-  public void setType(int newType) {
-    this.type = newType;
+  public int getTarget() {
+    return this.target;
   }
   
-  public void setSpeed(int newSpeed) {
+  public void setHp(int newHp) {
+    this.hp = newHp;
+  }
+  
+  public void setDamage(int newDamage) {
+    this.damage = newDamage;
+  }
+  
+  public void setMoneyValue(int newMoneyValue) {
+    this.moneyValue = newMoneyValue;
+  }
+  
+  public void setSpeed(double newSpeed) {
     this.speed = newSpeed;
   }
   
+  public void setTarget(int newTarget) {
+    this.target = newTarget;
+  }
+  
+  public void move(map_point[] path) {
+    double totalx = path[this.getTarget()].getX() - this.getX();
+    double totaly = path[this.getTarget()].getY() - this.getY();                                         
+    
+    double deg_alpha = Math.atan(totalx / totaly);
+    
+    double targetx = Math.sin(deg_alpha) * this.getSpeed();
+    double targety = Math.cos(deg_alpha) * this.getSpeed();
+    
+    boolean x_pass = false;
+    boolean y_pass = false;
+    if (totalx >= 0) {
+      this.setX(this.getX() + Math.abs(targetx));
+      if (this.getX() >= path[this.getTarget()].getX()) {
+        x_pass = true;
+      }
+    }
+    else {
+      this.setX(this.getX() - Math.abs(targetx));
+      if (this.getX() <= path[this.getTarget()].getX()) {
+        x_pass = true;
+      }
+    }
+    
+    if (totaly >= 0) {
+      this.setY(this.getY() + Math.abs(targety));
+      if (this.getY() >= path[this.getTarget()].getY()) {
+        y_pass = true;
+      }
+    }
+    else {
+      this.setY(this.getY() - Math.abs(targety));
+      if (this.getY() <= path[this.getTarget()].getY()) {
+        y_pass = true;
+      }
+    }        
+    
+    if (x_pass == true && y_pass == true) {
+      this.setTarget(this.getTarget() + 1);
+    }
+  }
 }
--- Tower.java
+++ Tower.java
@@ -1,12 +1,111 @@
-class Tower extends Objeckt{
-  private int type;
-  private float attackRange;
+import javafx.scene.image.Image;
+import java.util.ArrayList;
+
+class Tower extends Basis {
   
-  public int  gettype(){
-    return this.type;
+  private double attackRange;
+  private int attackDamage;
+  private int attackSpeed;
+  private double projectileSpeed;
+  private int cooldown;
+  
+  public Tower(Image sprite, double x, double y, double attackRange, int attackDamage, int attackSpeed, double projectileSpeed, int typeId) {
+    super(sprite, x, y);
+    this.attackRange = attackRange;
+    this.attackDamage = attackDamage;
+    this.attackSpeed = attackSpeed;
+    this.projectileSpeed = projectileSpeed;
+    this.cooldown = 0;
+    this.getIV().setFitHeight(100);
+    this.getIV().setFitWidth(100);
+    this.setX(this.getX()-50);
+    this.setY(this.getY()-50);
+    this.setTypeId(typeId);
   }
   
-  public float  getattackRange(){
+  public double getAttackRange() {
     return this.attackRange;
+  }
+  
+  public int getAttackDamage() {
+    return this.attackDamage;
+  }
+  
+  public int getAttackSpeed() {
+    return this.attackSpeed;
+  }
+  
+  public void setAttackRange(double newAttackRange) {
+    this.attackRange = newAttackRange;
+  }
+  
+  public void setAttackDamage(int newAttackDamage) {
+    this.attackDamage = newAttackDamage;
+  }
+  
+  public double getProjectileSpeed() {
+    return this.projectileSpeed;
+  }
+  
+  public void setProjectileSpeed(double newProjectileSpeed) {
+    this.projectileSpeed = newProjectileSpeed;
+  }
+  
+  public void setAttackSpeed(int newAttackSpeed) {
+    this.attackSpeed = newAttackSpeed;
+  }
+  
+  public int getCooldown() {
+    return this.cooldown;
+  }
+  
+  public void setCooldown(int newCooldown) {
+    this.cooldown = newCooldown;
+  }
+  
+  public Enemy checkIsEnemyInRange(ArrayList<Enemy> enemies){
+    for (int i = 0;i < enemies.size(); i++) {
+      double tmpX = enemies.get(i).getX();
+      double tmpY = enemies.get(i).getY();
+      // Compare radius of circle with
+      // distance of its center from
+      // given point
+      double distX;
+      double distY;
+      if (tmpX - this.getX() >= 0) {
+        distX = tmpX - this.getX();
+      }
+      else {
+        distX = this.getX() - tmpX;
+      }
+      
+      if (tmpY - this.getY() >= 0) {
+        distY = tmpY - this.getY();
+      }
+      else {
+        distY = this.getY() - tmpY;
+      }   
+      
+      double hypothenuse = Math.sqrt(Math.pow((distX), 2) + Math.pow((distY), 2));
+      //System.out.println("" + String.valueOf(hypothenuse));
+      if (hypothenuse <= this.getAttackRange()){
+        return enemies.get(i);
+      }
+      
+    }
+    return null;
+  }
+  
+  public boolean canAttack() {
+    if (this.cooldown == 0) {
+      return true;
+    }
+    return false;
+  }
+  
+  public void tickCooldown() {
+    if (this.cooldown > 0) {
+      this.cooldown--;
+    }
   }
 }
